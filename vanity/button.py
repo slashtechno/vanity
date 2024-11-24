f97ea3e1630f76fa636dc5e7f35d77a932f7a74a
slashtechno <77907286+slashtechno@users.noreply.github.com>
2024-11-24 04:12:29 -0600

    feat: animate button clicks!

--- vanity/button.py
+++ vanity/button.py
@@ -1,8 +1,14 @@
+import time
 from vanity.constants import *
 
 class Button:
     def __init__(self, x, y, text, func, args=()) -> None:
-        self.image = pygame.image.load("sprites/button-normal.png")
+        self.image_normal = pygame.image.load("sprites/button-normal.png")
+        self.image_pressed = pygame.image.load("sprites/button-pressed.png")
+        self.image = self.image_normal
+        self.last_click_time = 0  
+        self.press_duration = 0.5
+
 
         w, h = self.image.get_size()
 
@@ -23,17 +29,25 @@ class Button:
 
     def update(self, mx, my, click_this_frame):
         self.hover = False
-        self.clicked = False
+        # self.clicked = False
 
         if self.rect.collidepoint(mx, my):
             self.hover = True
         
         if self.hover and click_this_frame:
             self.clicked = True
-
+            self.last_click_time = time.time()  # Record the time of the click
             self.execute()
+        else:
+            # Check if the press duration has passed
+            if time.time() - self.last_click_time > self.press_duration:
+                self.clicked = False
     
     def draw(self, screen):
+        if self.clicked:
+            self.image = self.image_pressed
+        else:
+            self.image = self.image_normal
         screen.blit(self.image, (self.x, self.y))
 
         # pygame.draw.rect(screen, 'green' if self.hover else 'red', self.rect, 1)
