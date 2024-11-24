cb2c3811bd65b4c18771429d3a70cdb360622bf5
slashtechno <77907286+slashtechno@users.noreply.github.com>
2024-11-24 09:47:50 -0600

    feat!: get first video working

--- vanity/__main__.py
+++ vanity/__main__.py
@@ -4,6 +4,7 @@ import pygame
 import sys
 from vanity.button import Button
 from vanity.constants import FONT, ASSETS, SPRITES
+from pyvidplayer2 import Video, VideoPlayer
 
 # Initializing pygame
 pygame.init()
@@ -15,6 +16,9 @@ clock = pygame.time.Clock()
 # Defining the window we will display our game on (in terms of pixels)
 SCREEN_WIDTH = 650
 SCREEN_HEIGHT = 500
+# Before 3 do the video
+office_scenes = [0, 1, 2, 4, 5, 6]
+LAST_STEP = 6
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("vanity")
 
@@ -27,15 +31,17 @@ step = 0
 def increment_step(s: pygame.Surface = None):
     global step
     step += 1
-    if step > 5:
+    if step not in office_scenes:
         screen.fill((0, 0, 0))
+    if step > LAST_STEP:
+        ...
         
        
 
 def game_loop():
     """This function runs our main game loop, yippie!"""
     global frame, step, last_click_time
-    pygame.mixer.music.load(f"{ASSETS}/vanity.mp3")
+    pygame.mixer.music.load(f"  {ASSETS}/vanity.mp3")
     pygame.mixer.music.play(-1)
 
     greedy_button = Button(325, 430, None, lambda: increment_step(), ())
@@ -47,7 +53,8 @@ def game_loop():
         mx, my = pygame.mouse.get_pos()
         clicked = False
 
-        for event in pygame.event.get():
+        events = pygame.event.get()
+        for event in events:
             if event.type == pygame.QUIT:
                 running = False
                 pygame.quit()
@@ -58,7 +65,7 @@ def game_loop():
                     if current_time - last_click_time > click_cooldown:
                         clicked = True
                         last_click_time = current_time
-            if step > 5:
+            if step > LAST_STEP:
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_r:
                         step = 0
@@ -71,11 +78,11 @@ def game_loop():
         greedy_button.update(mx, my, clicked)
 
         screen.fill('black')
-        if (step <= 5):
-            office = pygame.image.load(f"{SPRITES}/office{step}.png")
+        if (step in office_scenes):
+            office = pygame.image.load(f"{SPRITES}/office{office_scenes.index(step)}.png")
             screen.blit(office, (0, 0))
             greedy_button.draw(screen)
-        elif step > 5 :
+        elif step > LAST_STEP:
             text = []
             text.append(FONT.render("All is vanity.", True, (255, 255, 255)))
             text.append(FONT.render("Press R to restart, or Q to quit.", True, (255, 255, 255)))
@@ -84,7 +91,14 @@ def game_loop():
                 text_rect = text_line.get_rect(center=(screen.get_width() / 2, y_offset))
                 screen.blit(text_line, text_rect)
                 y_offset += text_line.get_height() + 10  # Move down for the next line
-
+        else:
+            match step:
+                case 3:
+                    vid = Video(f"{ASSETS}/protest-fire.mp4")
+                    play_vid(vid, screen)
+                    step+=1
+                   
+                    
         if elapsed_time < 3:
             text = FONT.render("You have one job. Push the button.", True, (255, 255, 255))
             text_rect = text.get_rect(center=(screen.get_width() / 2, 40 + text.get_height() / 2))
@@ -92,6 +106,45 @@ def game_loop():
 
         pygame.display.flip()
 
-game_loop()
 
-# def is_button_pressed()
+def play_vid(vid, s):
+    vid.resize((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while vid.active:
+        key = None
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                vid.stop()
+                pygame.quit()
+                sys.exit(0)
+            elif event.type == pygame.KEYDOWN:
+                key = pygame.key.name(event.key)
+
+        if key == "r":
+            vid.restart()           #rewind video to beginning
+        elif key == "p":
+            vid.toggle_pause()      #pause/plays video
+        elif key == "m":
+            vid.toggle_mute()       #mutes/unmutes video
+        elif key == "right":
+            vid.seek(15)            #skip 15 seconds in video
+        elif key == "left":
+            vid.seek(-15)           #rewind 15 seconds in video
+        elif key == "up":
+            vid.set_volume(1.0)     #max volume
+        elif key == "down":
+            vid.set_volume(0.0)     #min volume
+
+        # only draw new frames, and only update the screen if something is drawn
+
+        if vid.draw(s, (0, 0), force_draw=False):
+            pygame.display.update()
+
+        pygame.time.wait(16) # around 60 fps
+    vid.close()
+
+
+def main():
+    game_loop()
+
+if __name__ == "__main__":
+    main()
